3bf9f902836d9e3d3f2fca23990c005d5aad22e5
nas.www.ay <nastaytalmazan@gmail.com>
2024-08-07 22:21:22 +1000

    Initial commit

---
+++ TestTask1221/TestTask1221App.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask1221App.swift
+//  TestTask1221
+//
+//  Created by Анастасия Талмазан on 2024-08-07.
+//
+
+import SwiftUI
+
+@main
+struct TestTask1221App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
